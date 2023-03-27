5ae240c09ab0bf99b9f2eb2d3b8e9ebd2915f280
YuraWin <yurawin@ukr.net>
2023-03-27 17:06:16 +0300

    1

---
+++ JS/script-test.js
@@ -0,0 +1,16 @@
+const age = 10;
+const totalPrice = 200.74;
+
+console.log(typeof 'Price');
+console.log(typeof 5);
+
+// alert('alarm');
+
+// const shouldRenew = confirm('Хотите продлить подписку?');
+// console.log(shouldRenew);
+
+let quantity = prompt('Введите кол-во товара');
+console.log(quantity);
+quantity = Number(quantity);
+console.log(typeof quantity);
+console.log(quantity);
